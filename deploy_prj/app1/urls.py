c735cfed498aa5cd252b9663cb5164a309b8f3d0
bea816 <kimnadon@naver.com>
2024-08-01 01:56:08 +0900

    db 테스트

--- deploy_prj/app1/urls.py
+++ deploy_prj/app1/urls.py
@@ -5,4 +5,10 @@ app_name = 'app1'
 
 urlpatterns = [
     path('', index, name="index"),
+    path('list', IndexView.as_view(), name='list'),
+    path('result/', result, name="result"),
+    path('create/', create, name = "create"),
+    path('detail/<int:id>', detail, name = "detail"),
+    path('update/<int:id>', update, name = "update"),
+    path('delete/<int:id>', delete, name = "delete"),
 ]
--- deploy_prj/app1/views.py
+++ deploy_prj/app1/views.py
@@ -1,4 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Phone
+from django.views.generic import ListView
 
 def index(request):
     return render(request, 'app1/index.html')
+
+class IndexView(ListView):
+    queryset = Phone.objects.all().order_by('name')
+    template_name = "app1/list.html"
+    context_object_name = 'posts'
+
+def result(request):
+    entered_text = request.GET['data']
+    posts = Phone.objects.all()
+
+    matchings = []
+
+    for post in posts:
+        if entered_text in post.name:
+            matchings.append(post)
+
+    return render(request, "app1/result.html", {'matchings': matchings, 'entered_text': entered_text})
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone_num = request.POST.get('phone_num')
+        email = request.POST.get('email')
+
+        post = Phone.objects.create(
+            name = name,
+            phone_num = phone_num,
+            email = email,
+        )
+        return redirect('app1:list')
+    return render(request, 'app1/create.html')
+
+def detail(request, id):
+    post = get_object_or_404(Phone, id = id)
+
+    return render(request, 'app1/detail.html', {'post' : post})
+
+def update(request, id):
+    post = get_object_or_404(Phone, id = id)
+    if request.method == "POST":
+        post.name = request.POST.get('name')
+        post.phone_num = request.POST.get('phone_num')
+        post.email = request.POST.get('email')
+        post.save()
+        return redirect('app1:list')
+    return render(request, 'app1/update.html', {'post' : post})
+
+def delete(request, id):
+    post = get_object_or_404(Phone, id = id)
+    if request.method == "POST":
+        post.delete()
+        return redirect('app1:list')
+    return render(request, 'app1/delete.html', {'post' : post})
