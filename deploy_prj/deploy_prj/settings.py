8cf17eb0d1c3e782efb9ee8d128110bfd5014fc0
bea816 <kimnadon@naver.com>
2024-07-31 20:55:51 +0900

    코드 수정

--- deploy_prj/deploy_prj/settings.py
+++ deploy_prj/deploy_prj/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'deploy_prj.urls'
